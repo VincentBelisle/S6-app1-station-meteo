0d327993d7f02899f8c61a4fe19af26db5c129b4
Elliot Gaulin <elliotgaulin@hotmail.com>
2026-05-06 12:39:41 -0400

    some stuff works

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,10 @@
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#include <Adafruit_DPS310.h>
 
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
@@ -9,9 +13,34 @@
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
+#define DHTTYPE DHT11
+#define DHTPIN 16
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
+Adafruit_DPS310 dps;
+uint32_t delayMS;
+
+
 void setup() {
   Serial.begin(115200);
   Serial.println("Starting BLE work!");
+  dht.begin();
+
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  delayMS = sensor.min_delay / 1000;
+
+  Serial.println("DPS310");
+  if (!dps.begin_I2C()) {
+    Serial.println("Failed to find DPS");
+    while (1)
+      yield();
+  }
+  Serial.println("DPS OK!");
+
+  // Setup highest precision
+  dps.configurePressure(DPS310_64HZ, DPS310_64SAMPLES);
+  dps.configureTemperature(DPS310_64HZ, DPS310_64SAMPLES);
 
   BLEDevice::init("MyESP32");
   BLEServer *pServer = BLEDevice::createServer();
@@ -28,6 +57,7 @@ void setup() {
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->setScanResponse(true);
+
   pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
   pAdvertising->setMinPreferred(0x12);
   BLEDevice::startAdvertising();
@@ -36,5 +66,46 @@ void setup() {
 
 void loop() {
   Serial.printf("cuille \n");
-  delay(2000);
+
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println("Error reading temperature!");
+  }
+  else {
+    Serial.print("Temperature: ");
+    Serial.print(event.temperature);
+    Serial.println(" *C");
+  }
+
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println("Error reading humidity!");
+  }
+  else {
+    Serial.print("Humidity: ");
+    Serial.print(event.relative_humidity);
+    Serial.println(" %");
+  }
+
+  // DPS310 barometre: lire les deux mesures sur le meme echantillon.
+  sensors_event_t temp_event, pressure_event;
+
+  if (dps.temperatureAvailable() || dps.pressureAvailable()) {
+    dps.getEvents(&temp_event, &pressure_event);
+
+    Serial.print(F("Temperature = "));
+    Serial.print(temp_event.temperature);
+    Serial.println(" *C");
+
+    Serial.print(F("Pressure = "));
+    Serial.print(pressure_event.pressure);
+    Serial.println(" hPa");
+
+    Serial.println();
+  } else {
+    Serial.println("DPS310 data not ready");
+  }
+
+  delay(delayMS);
 }
